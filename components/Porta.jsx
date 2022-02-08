ea6dac85c80f17bf2509745b53c547cb8dd1760b
Alessandro Pereira Rodrigues <ale.per.rod@gmail.com>
2022-02-08 12:25:38 -0300

    Segundo commit.

--- components/Porta.jsx
+++ components/Porta.jsx
@@ -2,8 +2,15 @@ import styles from '../styles/Porta.module.css'
 
 export default function Porta(props){
     return(
-        <div className={styles.porta}>
-            <div className={styles.numero}>3</div>
+        <div className={styles.area}>
+            <div className={styles.umbral}>
+                <div className={styles.porta}>
+                    <div className={styles.numero}>3</div>
+                </div>
+            </div>
+            <div className={styles.chao}>
+
+            </div>
         </div>
     )
 }
