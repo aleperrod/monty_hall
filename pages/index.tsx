96c0ab1186cab014577362b4cf7a4ec08af4805c
Alessandro Pereira Rodrigues <ale.per.rod@gmail.com>
2022-02-10 08:01:14 -0300

    Quarto commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Porta from "../components/Porta";
+import PortaModel from "../modelo/porta";
+
+export default function Home() {
+  const [p1,setP1] = useState(new PortaModel(1))
+  
+  return (
+    <div style={{display: 'flex'}}>
+      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
+    </div>
+  )
+}
