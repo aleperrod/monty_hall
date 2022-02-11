bc7816689a04c906d94addcfd4b4a7d9420e61a4
Alessandro Pereira Rodrigues <ale.per.rod@gmail.com>
2022-02-11 10:14:03 -0300

    Quinto commit

--- components/Porta.tsx
+++ components/Porta.tsx
@@ -1,5 +1,6 @@
 import PortaModel from '../modelo/porta'
 import styles from '../styles/Porta.module.css'
+import Presente from './Presente'
 
 interface PortaProps {
     value: PortaModel
@@ -9,17 +10,27 @@ interface PortaProps {
 
 export default function Porta(props: PortaProps){
     const porta = props.value
-    const selecionada = porta.selecionada ? styles.selecionada : ''
+    const selecionada = porta.selecionada && !porta.aberta ? styles.selecionada : ''
     
     const alternarSelecao = e => props.onChange(porta.alternarSelecao())
+    const abrir = e => {
+        e.stopPropagation()
+        props.onChange(porta.abrir())
+    }
 
+    function renderizarPorta(){
+        return(
+                <div className={styles.porta}>
+                    <div className={styles.numero}>{porta.numero}</div>
+                    <div className={styles.macaneta} onClick={abrir}></div>
+                </div>
+        )
+    }
     return(
         <div className={styles.area} onClick={alternarSelecao}>
             <div className={`${styles.umbral} ${selecionada}`}>
-                <div className={styles.porta}>
-                    <div className={styles.numero}>{porta.numero}</div>
-                    <div className={styles.macaneta}></div>
-                </div>
+                {porta.fechada ? renderizarPorta() :
+                    porta.temPresente ? <Presente/> : false}
             </div>
             <div className={styles.chao}>
 
